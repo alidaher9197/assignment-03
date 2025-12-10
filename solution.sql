ca4da7419af3889e3c3188408436bb982a50926d
alidaher9197 <alidaher9197@gmail.com>
2025-12-10 23:36:51 +0200

    edit column grade

--- solution.sql
+++ solution.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE school_db;
 CREATE TABLE students_table(
     student_id INT PRIMARY KEY,
-    first_name VARCHAR(50)
+    first_name VARCHAR(50),
     last_name VARCHAR(50),
     age INT,
     grade DECIMAL(5,2),
