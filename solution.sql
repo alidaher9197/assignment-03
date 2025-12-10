8cea8647a669dd7410707d7a36c196d041e15f45
alidaher9197 <alidaher9197@gmail.com>
2025-12-11 00:05:56 +0200

    insert values to table students_table

--- solution.sql
+++ solution.sql
@@ -7,3 +7,18 @@ CREATE TABLE students_table(
     grade DECIMAL(5,2),
     city VARCHAR(50)
 );
+ALTER TABLE students_table
+CHANGE COLUMN grade grade VARCHAR(1);
+INSERT INTO students_table
+(student_id,first_name,last_name,age,grade,city) 
+VALUES
+(1, "Ahmad", "Ahmad", 17, "A", "Beirut"),
+(2, "Sara", "Khatib", 18, "B", "Tripoli"),
+(3, "Hassan", "Zein", 17, "A", "Sidon"),
+(4, "Mariam", "Ata", 19, "C", "Beirut"),
+(5, "Khaled", "Najjar", 16, "B", "Tyre"),
+(6, "Reem", "Salem", 18, "A", "Tripoli"),
+(7, "Youssef", "Hany", 17, "C", "Sidon"),
+(8, "Hiba", "Zein", 20, "B", "Beirut"),
+(9, "Fadi", "Hashem", 16, "A", "Tyre"),
+(10, "Layla", "Hassan", 17, "B", "Tripoli");
