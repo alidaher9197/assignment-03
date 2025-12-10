b47b8aea99be028d00b459a70c4e2069f0a3ae68
alidaher9197 <alidaher9197@gmail.com>
2025-12-11 01:11:44 +0200

    change city from tripoli to beirut

--- solution.sql
+++ solution.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE school_db;
+
 CREATE TABLE students_table(
     student_id INT PRIMARY KEY,
     first_name VARCHAR(50),
@@ -7,8 +8,10 @@ CREATE TABLE students_table(
     grade DECIMAL(5,2),
     city VARCHAR(50)
 );
+
 ALTER TABLE students_table
 CHANGE COLUMN grade grade VARCHAR(1);
+
 INSERT INTO students_table
 (student_id,first_name,last_name,age,grade,city) 
 VALUES
@@ -22,20 +25,61 @@ VALUES
 (8, "Hiba", "Zein", 20, "B", "Beirut"),
 (9, "Fadi", "Hashem", 16, "A", "Tyre"),
 (10, "Layla", "Hassan", 17, "B", "Tripoli");
-SELECT * FROM students_table;
-SELECT first_name,city FROM students_table;
-SELECT * FROM students_table WHERE grade="B";
-SELECT * FROM students_table WHERE city!="Beirut";
-SELECT * FROM students_table WHERE age>17;
-SELECT * FROM students_table WHERE age>=18;
-SELECT * FROM students_table WHERE student_id<5;
-SELECT * FROM students_table WHERE age<=16;
-SELECT * FROM students_table WHERE city IS NULL;
-SELECT * FROM students_table WHERE city IS NOT NULL;
-SELECT * FROM students_table WHERE age BETWEEN 17 AND 19;
-SELECT * FROM students_table WHERE age>17 AND grade="A";
-SELECT * FROM students_table WHERE city="Beirut" OR city="Tripoli";
+
+SELECT * 
+FROM students_table;
+
+SELECT first_name,city 
+FROM students_table;
+
+SELECT * 
+FROM students_table 
+WHERE grade="B";
+
+SELECT * 
+FROM students_table 
+WHERE city!="Beirut";
+
+SELECT * 
+FROM students_table 
+WHERE age>17;
+
+SELECT * 
+FROM students_table 
+WHERE age>=18;
+
+SELECT * 
+FROM students_table 
+WHERE student_id<5;
+
+SELECT * 
+FROM students_table 
+WHERE age<=16;
+
+SELECT * 
+FROM students_table 
+WHERE city IS NULL;
+
+SELECT * 
+FROM students_table 
+WHERE city IS NOT NULL;
+
+SELECT * 
+FROM students_table 
+WHERE age BETWEEN 17 AND 19;
+
+SELECT * 
+FROM students_table 
+WHERE age>17 AND grade="A";
+
+SELECT * 
+FROM students_table 
+WHERE city="Beirut" OR city="Tripoli";
+
 UPDATE students_table
 SET grade="A"
-WHERE
-student_id=4;
+WHERE student_id=4;
+
+UPDATE students_table
+SET city="Beirut"
+WHERE city="Tripoli";
