9d992527ed92288319a2a0b109b3c128db4029bd
alidaher9197 <alidaher9197@gmail.com>
2025-12-10 23:32:14 +0200

    creating table student_table

--- solution.sql
+++ solution.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE school_db;
+CREATE TABLE students_table(
+    student_id INT PRIMARY KEY,
+    first_name VARCHAR(50)
+    last_name VARCHAR(50),
+    age INT,
+    grade DECIMAL(5,2),
+    city VARCHAR(50)
+);
